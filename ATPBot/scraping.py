6a9bca373abcd6a98115d73866c7d226587e9666
cvlolo <lolocasado1@gmail.com>
2017-11-03 00:00:43 +0100

    Prueba db

--- ATPBot/scraping.py
+++ ATPBot/scraping.py
@@ -23,6 +23,7 @@ def scrapClasificacion():
 def mostrarClasificacion():
 	players=db.mostrar_jugadores()
 	points=db.mostrar_puntos()
+	string=""
 	for i in range(0,len(players)):
 		string= "En la posicion " +str(i)+ "esta "+ players[0] + "con: " +points[1]+ " puntos" +"\n" + string
 	return string
