dd975666adc4e0bce680ec5b4db8d01d285fe9dd
cvlolo <lolocasado1@gmail.com>
2017-11-02 18:32:02 +0100

    Prueba db

--- ATPBot/db.py
+++ ATPBot/db.py
@@ -11,4 +11,4 @@ url = urlparse.urlparse(os.environ["DATABASE_URL"])
 def insertar_jugador(nombre,puntos):
 	conn = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
 	cursor = conn.cursor()
-	cursor.execute("Insert into jugadores(nombre, puntos) values (%s, %d)", (nombre, puntos))
+	cursor.execute("Insert into jugadores(nombre, puntos) values (%s, %s);", (nombre, puntos))
