e660eab8e6804189cf9815162a34fbaeb703161a
cvlolo <lolocasado1@gmail.com>
2017-11-02 11:55:00 +0100

    Subo version primera del bot

---
+++ ATPBot/clasePrueba.py
@@ -0,0 +1,20 @@
+
+
+
+dictionary = {"Roger Federer":7505,"Andy Murray":6290, "Alexander Zverev":4400, "Rafael Nadal":9875}
+
+def extraerPuntuacion(Nombre):
+	return dictionary.get(Nombre)
+
+
+def mostrarRanking():
+	clasif = sorted(dictionary.items(), key=lambda dictionary: dictionary[1])
+	string = ""
+	for x in clasif:
+		string="Nombre: " + x[0] + " Puntuacion: " +str(x[1])+ "\n" + string
+		
+	return string
+
+	
+
+
