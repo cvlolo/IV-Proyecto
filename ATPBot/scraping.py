2803827757edc353af13608309853418e4f1269b
cvlolo <lolocasado1@gmail.com>
2017-11-02 19:39:19 +0100

    Prueba db

---
+++ ATPBot/scraping.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+import re
+import db
+
+req = requests.get('http://www.atpworldtour.com/en/rankings/singles')
+soup = BeautifulSoup(req.text, "html5lib")
+
+def scrapClasificacion():
+	entradas = soup.find_all('td', {'class' : 'player-cell'}, 'a')
+	puntos = soup.find_all('td', {'class' : 'points-cell'}, 'a')
+	patron = re.compile('data-ga-label="(.*?)"')
+
+	nombres= patron.findall(str(entradas))
+
+	patron = re.compile('singles\">(.*?)<')
+	pts = patron.findall(str(puntos))
+
+	for x in range(len(nombres)):
+		db.insertar_jugador(nombres[x],pts[x])
+
+	
