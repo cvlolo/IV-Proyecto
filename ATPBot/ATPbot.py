0584db1199b9ecec8c88687fd23adbd6a91aeed0
cvlolo <lolocasado1@gmail.com>
2017-11-03 00:24:02 +0100

    Prueba db

--- ATPBot/ATPbot.py
+++ ATPBot/ATPbot.py
@@ -23,6 +23,14 @@ def clasificacion(m):
 	scraping.scrapClasificacion()
 	mensaje=scraping.mostrarClasificacion()
 	bot.send_message( cid, mensaje)
+
+bot.set_update_listener(listener)
+
+@bot.message_handler(commands=['help']) 
+def help(m): 
+	cid = m.chat.id 
+	mensaje="¡Buenas amante de la ATP!,este bot te proporcionara informacion de los ultimos eventos relacionados con la ATP, usa el comando clasificacion para obtener los 50 mejores tenistas del momento junto a su puntuacion." 
+	bot.send_message( cid, mensaje)
 	
 
 
