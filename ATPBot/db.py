c1e3abd6df57a010fd457107acb354cd7c5feebf
cvlolo <lolocasado1@gmail.com>
2017-11-02 20:09:13 +0100

    Prueba db

--- ATPBot/db.py
+++ ATPBot/db.py
@@ -20,7 +20,7 @@ def consultar_jugador(nombre):
 	conn = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
 	cursor = conn.cursor()
 	try:
-		cursor.execute("select nombre from jugadores where nombre=%s);", nombre)
+		cursor.execute("select nombre from jugadores where nombre=%s);", (nombre))
 	except psycopg2.Error as e:
 		conn.commit()
 		cursor.close()
