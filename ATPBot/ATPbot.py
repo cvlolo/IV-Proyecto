ff7998bfaaff51f34eda31cd3ef71d41b4c43460
cvlolo <lolocasado1@gmail.com>
2017-11-02 11:37:27 +0100

    Subo version primera del bot

---
+++ ATPBot/ATPbot.py
@@ -0,0 +1,26 @@
+import telebot 
+from telebot import types 
+from telebot import util
+import time 
+import os
+
+TOKEN = os.environ['TOKEN'] 
+bot = telebot.TeleBot(TOKEN)
+
+def listener(messages):
+	for m in messages: 
+        	if m.content_type == 'text': 
+			cid = m.chat.id 
+		    	print ("[" + str(cid) + "]: " + m.text) 
+
+bot.set_update_listener(listener)
+
+@bot.message_handler(commands=['helloworld']) 
+def clasificacion(m): 
+	cid = m.chat.id 
+	mensaje="Bienvenido al bot de la ATP"
+	bot.send_message( cid, mensaje)
+
+bot.set_update_listener(listener)
+
+bot.polling(none_stop=True)
