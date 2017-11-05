03d1fe2570b9f4b725e5ef20bc9c54d086a680bc
cvlolo <lolocasado1@gmail.com>
2017-11-05 16:08:04 +0100

    Actualizo test

---
+++ ATPBot/funVarias.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+import re 
+
+ATP_datos = {'Nadal': 1020 , 'Federer' : 980, 'Djokovik': 650}
+
+
+def ObtenerPuntosJugador(jugador):
+        return ATP_datos.get(jugador)
+
+def insertarJugador(nombre,puntos):
+	ATP_datos[nombre]=puntos
+
+def borrarJugador(nombre,puntos):
+	del ATP_datos[nombre]=puntos
+
+
+def ATPClas():
+        entradas = soup.find_all('td', {'class' : 'player-cell'}, 'a')
+	puntos = soup.find_all('td', {'class' : 'points-cell'}, 'a')
+	patron = re.compile('data-ga-label="(.*?)"')
+
+	nombres= patron.findall(str(entradas))
+
+	patron = re.compile('singles\">(.*?)<')
+	pts = patron.findall(str(puntos))
+        string = ""
+        for x in range(len(nombres)):
+                 string="En la posicion " +str(i)+ " esta "+ players[i-1] + " con: " +points[i-1]+ " puntos" +"\n" + string
+        return string
+
--- ATPBot/testing.py
+++ ATPBot/testing.py
@@ -1,28 +1,23 @@
-
-import db
-import scraping
+import funVarias.py
 import unittest
 
-class Database(unittest.TestCase):
-	def testConsultar(self):
-		error=db.consultar_jugador("Rafael Nadal")
-		self.assertEqual(error,-1)
+class Test(unittest.TestCase):
+        
+        def test_CompIn(self): 
+                funVarias.insertarJugador("Pepe",10)
+		ptos=funVarias.ObtenerPuntosJugador("Pepe")
+		self.assertEqual(ptos,10)
 
-	def testInsertar(self):
-		lista=[]
-		db.insertar_jugador("Vardy","15")
-		lista=db.consultar_jugador("Vardy")
-		print(lista)
-		self.assertEqual(lista[0], "Vardy")
+	def test_CompOut(self): 
+                funVarias.borrarJugador("Pepe")
+		ptos=funVarias.ObtenerPuntosJugador("Pepe")
+		self.assertFalse(ptos,10)
+		
 
-	def testBorrar(self):
-		lista=[]
-		db.borrar_jugador("Vardy")
-		lista=db.consultar_jugador("Vardy")
-		self.assertEqual(lista,-1)
-		
-		
-		
-		
+
+        def test_CompClas(self):
+                self.assertTrue(len(funVarias.ATPClas()) > 0)  #Compruebo que el tamanio de la lista de la clasificacion es 
+                                                                            #4 ya que de momento tengo 4 equipos
+
 if __name__ == '__main__':
-    unittest.main()
+        unittest.main()
