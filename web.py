214b3a22b49a85fcc6d793ebe88ed475f7b3c373
cvlolo <lolocasado1@gmail.com>
2017-11-20 19:46:57 +0100

    Cambio status

--- web.py
+++ web.py
@@ -14,6 +14,11 @@ def status():
     js = {"status": "OK"}
     return json.dumps(js)
 
+@app.route("/status")
+def status():
+    js = {"status": "OK"}
+    return json.dumps(js)
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
