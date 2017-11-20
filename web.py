c9f8f35e18f35dca2068958b90f49a85525f8567
cvlolo <lolocasado1@gmail.com>
2017-11-20 20:55:43 +0100

    Arrglo ruta

--- web.py
+++ web.py
@@ -4,21 +4,36 @@ from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
 
-{
-   "status": "OK"
-}
+class status(Resource):
+    def get(self):
+        schema = {
+           "status": "OK"
+        }
+        return jsonify(schema)
 
-@app.route("/")
-def status():
-    js = {"status": "OK"}
-    return jsonify(js)
+api.add_resource(status, '/')
 
-@app.route("/status")
-def st():
-    js = {"status": "OK"}
-    return jsonify(js)
+class statusDocker(Resource):
+    def get(self):
+        schema = {
+           "status": "OK"
+        }
+        return jsonify(schema)
+
+api.add_resource(statusDocker, '/status')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+	app.run(debug=True, use_reloader=True)
+
+
+
+
+
+
+
+
+
+
+
