df5cf336a9c537cd4a1e50ecab1f043be6ff8025
cvlolo <lolocasado1@gmail.com>
2018-01-14 00:23:52 +0100

    Añado carpeta puppet junto al archivo .pp donde se le indican los recursos a puppet para aprovisionar la maquina de vagrant #19

---
+++ puppet/manifests/recursos.pp
@@ -0,0 +1,37 @@
+# Actualizo los repositorios existentes
+
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update"
+}
+
+# Instalación de libpq
+package { "libpq-dev":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+# Instalación de git
+package { "git":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+# Instalación de pip
+package { "python-pip":
+    ensure => present,
+    require => Exec["apt-get update"]
+}
+
+#Clonamos repositorio
+vcsrepo { "/home/ubuntu/":
+  ensure => latest,
+  provider => git,
+  require => [ Package[ 'git' ] ],
+  source => "https://github.com/cvlolo/IV-Proyecto.git",
+  revision => 'master',
+}
+
+#Instalamos requirements
+exec { "requirements":
+    command => "/usr/bin/pip install -r  home/ubuntu/IV-Proyecto/requirements.txt
+}
