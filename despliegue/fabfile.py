b6eacb7cd1c5d9360642796f893f4058e49a089c
cvlolo <lolocasado1@gmail.com>
2018-01-13 16:47:26 +0100

    Actualizo archivos

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -6,7 +6,7 @@ def install():
 	run('pip install -r /home/ubuntu/IV-Proyecto/requirements.txt')
 
 def web():
-    run('cd ./Proyecto-IV && sudo chmod +x script.sh && sudo sh script.sh' )
+    run('cd ./IV-Proyecto && sudo chmod +x script.sh && sudo sh script.sh' )
 
 def atpbot():
     run('sudo python home/ubuntu/IV-Proyecto/ATPBot/ATPbot.py')
