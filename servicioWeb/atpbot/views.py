2b51d6e74156f431c598369d5539cbc85b1b42b9
cvlolo <lolocasado1@gmail.com>
2018-01-13 23:08:33 +0100

    Borro un espacio para poder referenciar el issue que olvide en el commit anterior #19

--- servicioWeb/atpbot/views.py
+++ servicioWeb/atpbot/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def root(request):
-	 data={"status":"OK"}
-	 return JsonResponse(data,safe=False)
+	data={"status":"OK"}
+	return JsonResponse(data,safe=False)
 
 def status(request):
-	 data={"status":"OK"}
-	 return JsonResponse(data,safe=False)
+	data={"status":"OK"}
+	return JsonResponse(data,safe=False)
