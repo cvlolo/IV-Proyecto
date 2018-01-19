3eb565752f6d1545010aeab9c8503eb1ca2eda5f
cvlolo <lolocasado1@gmail.com>
2018-01-19 04:00:11 +0100

    Se actualiza el script con todas las opciones asi como con las variables externas del vagrantfile, cierro issue olvidado Closes #24

--- provision/ScriptAutomatizacion.sh
+++ provision/ScriptAutomatizacion.sh
@@ -1,7 +1,31 @@
-vagrant up --provider=aws
+ACCES_KEY="tuclave" SECRET_ACCES_KEY="tuclavesecreta" TOKEN="tu_token-access" vagrant up --provider=aws
 
-fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS install
-fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS web
 
-#Cambiar tuclave.pem por el archivo correspondiente de AWS
-#Cambiar DNS por el correspondiente de la instancia creada con Vagrant
+
+
+#El despliegue quedará realizado por completo con los siguientes comandos:
+
+fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS Install
+fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS RunDosRun
+
+
+#Pueden desplegarse los dos servicios, web y bot de forma individual con:
+
+#fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS DjangoUp
+#fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS botUp
+
+#Para acabar con el proceso de gunicorn utilizar:
+
+#fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS killGunicorn
+
+#Para eliminar el despliegue:
+
+#fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS Uninstall
+
+#Para saber si django posee algun fallo:
+
+#fab -i tuclave.pem -f rutaFabFile -H ubuntu@DNS TestDjango
+
+#tuclave.pem hace referencia a la clave creada desde AWS para acceder por SSH a la instancia
+#Utiliza el DNS de la misma instancia
+# rutaFabFile sera del tipo /ruta/hacia/fabfile.py
