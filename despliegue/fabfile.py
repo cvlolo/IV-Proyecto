95d7426fecef1f25af76c5694154766ad85febc0
cvlolo <lolocasado1@gmail.com>
2018-01-19 03:43:35 +0100

    Fabfile terminado y testeado, se elimina archivo de configuracion en django pues no hace falta Closes #26 #24

---
+++ despliegue/fabfile.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+def Install():
+	with settings(warn_only=True):
+		if run("test -d ~/IV-Proyecto").failed:
+				run("git clone https://github.com/cvlolo/IV-Proyecto.git")
+
+	run("sudo apt-get update")
+	run('sudo apt-get install -y python')
+	run('sudo apt-get install -y python-pip')
+	run('sudo apt-get install -y libpq-dev')
+	run('pip install -r ~/IV-Proyecto/requirements.txt')
+
+def Uninstall():
+	with settings(warn_only=False):
+		if run("test -d ~/IV-Proyecto").succeeded:	
+			run('sudo rm -rfv ./IV-Proyecto')
+
+def TestDjango():
+    with settings(warn_only=True):
+        result = local('cd ~/IV-Proyecto/servicioWeb && ./manage.py test atpbot', capture=True)
+    if result.failed and not confirm("Parece que algo anda mal con la aplicacion.¿Seguro que desea realizar el despliegue?"):
+        abort("Abortando despliegue")
+
+def DjangoUp():
+	TestDjango()
+	run('cd ~/IV-Proyecto/servicioWeb/ && gunicorn servicioWeb.wsgi -b 0.0.0.0:2000')
+
+def botUp():
+	run('cd ~/IV-Proyecto/ATPBot/ && python ATPbot.py')
+
+def RunDosRun():
+	botUp()
+	DjangoUp()
+
+def killGunicorn():
+	run('ps ax|grep gunicorn')
+    	run('sudo pkill gunicorn')
