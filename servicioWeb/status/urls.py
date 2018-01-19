baccc78da375a217db800b91510bef6f32bf5b96
cvlolo <lolocasado1@gmail.com>
2018-01-19 11:29:44 +0100

    Se instala aplicacion y se borra la ruta status/status de las urls #19

--- servicioWeb/status/urls.py
+++ servicioWeb/status/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from status import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.root, name='root'),
-	url(r'^status', views.status, name='status')
+        url(r'^$', views.root, name='root')
 
 )
