f984e76940541a57f403b70b5d57c661341ee6a8
cvlolo <lolocasado1@gmail.com>
2018-01-19 11:23:21 +0100

    No hacen falta ni los import ni una ruta status dentro de status, seria una redundancia sin sentido #19

--- servicioWeb/status/views.py
+++ servicioWeb/status/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import db
-import scraping
 
 def root(request):
 	data={"status":"OK"}
 	return JsonResponse(data,safe=False)
 
-def status(request):
-	data={"status":"OK"}
-	return JsonResponse(data,safe=False)
 
 
