b2bb79f6a318f4e8692b3e2e1c321e554a9e76fe
cvlolo <lolocasado1@gmail.com>
2018-01-19 01:37:39 +0100

    Añado un test muy simple en django para saber que la estructura no tiene fallos #24

--- servicioWeb/atpbot/tests.py
+++ servicioWeb/atpbot/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class DjangoTestCase(TestCase):
+    def test_status(self):
+        response = self.client.get(reverse('root'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "status")
+	
+
